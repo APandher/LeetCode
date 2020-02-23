bbcaabffc47e0ea295c74fba603ab7865268c798
Pandher <pandha1@mcmaster.ca>
2020-02-22 22:17:47 -0500

    Finished question in O(n)

--- Python/InterviewQuestions-Easy/Array/RemoveDuplicatesFromSortedArray.py
+++ Python/InterviewQuestions-Easy/Array/RemoveDuplicatesFromSortedArray.py
@@ -1,18 +1,32 @@
 
-nums = [0,0,1,1,1,2,2,3,3,4]
-
-# Attempt #1: Brute Force 
+# Attempt #1: Brute Force O(n^2)
 # Notable Mention 1: Python count method counts the number of instances of an element in an array
 # Notable Mention 2: Python index method returns the index of the first matched element found in the array  
-def removeDuplicates(nums):
-    for i in nums:
-        while nums.count(i) > 1:
-            indexNum = nums.index(i)
+
+def removeDuplicates(self, nums: List[int]) -> int:
+    for num in nums:
+        while nums.count(num) > 1:
+            indexNum = nums.index(num)
             indexNum += 1
             nums.remove(nums[indexNum])
 
     return len(nums)
 
 
-print(removeDuplicates(nums))
+# Attempt #2 O(n) 
+# Notable Mention 1: Two pointer method can reduce the need for an extra loop
+# Notable Mention 2: del method can be used to delete a specific index in an array
 
+def removeDuplicates(self, nums: List[int]) -> int:
+    i = 1
+    j = 0
+
+    while i < len(nums):
+        if nums[i] == nums[j]:
+            del nums[j]
+        else:
+            i += 1
+            j += 1
+
+    return len(nums)
+        
