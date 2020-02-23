bf1fff68922f6ae818f85a85aab52049df6d419f
Pandher <pandha1@mcmaster.ca>
2020-02-22 21:32:02 -0500

    Adding brute force solution

--- Python/InterviewQuestions-Easy/Array/RemoveDuplicatesFromSortedArray.py
+++ Python/InterviewQuestions-Easy/Array/RemoveDuplicatesFromSortedArray.py
@@ -1,4 +1,18 @@
 
-nums = [1,1,2]
+nums = [0,0,1,1,1,2,2,3,3,4]
 
+# Attempt #1: Brute Force 
+# Notable Mention 1: Python count method counts the number of instances of an element in an array
+# Notable Mention 2: Python index method returns the index of the first matched element found in the array  
 def removeDuplicates(nums):
+    for i in nums:
+        while nums.count(i) > 1:
+            indexNum = nums.index(i)
+            indexNum += 1
+            nums.remove(nums[indexNum])
+
+    return len(nums)
+
+
+print(removeDuplicates(nums))
+
